9d2110cbb65589e3ce54d708af4d261788d2d85a
Aleksey Filippov <sarum9in@gmail.com>
2012-11-24 19:02:25 +0400

    process test suite refactoring.

--- tests/process.cpp
+++ tests/process.cpp
@@ -1,4 +1,6 @@
-#include "bunsan/util.hpp"
+#define BOOST_TEST_MODULE process
+#include <boost/test/unit_test.hpp>
+
 #include "bunsan/process/context.hpp"
 
 #include <boost/archive/text_iarchive.hpp>
@@ -6,35 +8,35 @@
 
 #include <sstream>
 
-#include <cassert>
+BOOST_AUTO_TEST_SUITE(process)
 
-int main()
+BOOST_AUTO_TEST_SUITE(context)
+
+BOOST_AUTO_TEST_CASE(exceptions)
 {
-    // exceptions
     bunsan::process::context ctx;
-    try
-    {
-        bunsan::process::context ctx;
-        ctx.use_path();
-    }
-    catch (std::invalid_argument &e)
-    {
-        SLOG("caught: "<<e.what());
-    }
-    // serialization
-    {
-        ctx.executable("sdnakdn");
-        ctx.argv({"hello", "world"});
-    }
+    BOOST_CHECK_THROW(ctx.use_path(), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_CASE(serialization)
+{
+    bunsan::process::context ctx;
+    ctx.executable("sdnakdn");
+    ctx.argv({"hello", "world"});
+
     std::stringstream buf;
     {
         boost::archive::text_oarchive oa(buf);
-        oa<<ctx;
+        oa << ctx;
     }
     {
         bunsan::process::context ctx_;
         boost::archive::text_iarchive ia(buf);
-        ia>>ctx_;
-        assert(ctx==ctx_);
+        ia >> ctx_;
+        BOOST_CHECK(ctx == ctx_);
     }
 }
+
+BOOST_AUTO_TEST_SUITE_END() // context
+
+BOOST_AUTO_TEST_SUITE_END() // process
