4ae84081e6db13ac321e7746250646d679edfc8c
Aleksey Filippov <sarum9in@gmail.com>
2013-10-30 12:36:47 +0400

    {check_,}sync_execute() declaration style fix.

--- include/bunsan/process/execute.hpp
+++ include/bunsan/process/execute.hpp
@@ -10,37 +10,34 @@
 #include <string>
 #include <vector>
 
-namespace bunsan
+namespace bunsan{namespace process
 {
-    namespace process
+    int sync_execute(const context &ctx);
+
+    int sync_execute(context &&ctx);
+
+    inline int sync_execute(
+        const boost::filesystem::path &cwd,
+        const boost::filesystem::path &executable,
+        const std::vector<std::string> &arguments,
+        bool use_path=true)
     {
-        int sync_execute(const context &ctx);
-
-        int sync_execute(context &&ctx);
-
-        inline int sync_execute(
-            const boost::filesystem::path &cwd,
-            const boost::filesystem::path &executable,
-            const std::vector<std::string> &arguments,
-            bool use_path=true)
-        {
-            return sync_execute(context().current_path(cwd).executable(executable).arguments(arguments).use_path(use_path));
-        }
-
-        inline int sync_execute(
-            const boost::filesystem::path &cwd,
-            const std::vector<std::string> &arguments,
-            bool use_path=true)
-        {
-            return sync_execute(context().current_path(cwd).arguments(arguments).use_path(use_path));
-        }
-
-        template <typename ... Args>
-        void check_sync_execute(Args &&...args)
-        {
-            int exit_status_ = sync_execute(std::forward<Args>(args)...);
-            if (exit_status_)
-                BOOST_THROW_EXCEPTION(bunsan::process::non_zero_exit_status_error(exit_status_));
-        }
+        return sync_execute(context().current_path(cwd).executable(executable).arguments(arguments).use_path(use_path));
     }
-}
+
+    inline int sync_execute(
+        const boost::filesystem::path &cwd,
+        const std::vector<std::string> &arguments,
+        bool use_path=true)
+    {
+        return sync_execute(context().current_path(cwd).arguments(arguments).use_path(use_path));
+    }
+
+    template <typename ... Args>
+    void check_sync_execute(Args &&...args)
+    {
+        int exit_status_ = sync_execute(std::forward<Args>(args)...);
+        if (exit_status_)
+            BOOST_THROW_EXCEPTION(bunsan::process::non_zero_exit_status_error(exit_status_));
+    }
+}}
