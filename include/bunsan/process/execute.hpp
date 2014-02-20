543c691f151654e52be4286e17d24cc5928db739
Aleksey Filippov <sarum9in@gmail.com>
2014-02-20 18:39:44 +0400

    bunsan/process/execute.hpp style fixes.

--- include/bunsan/process/execute.hpp
+++ include/bunsan/process/execute.hpp
@@ -6,8 +6,8 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/optional.hpp>
 
-#include <functional>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace bunsan{namespace process
@@ -22,7 +22,13 @@ namespace bunsan{namespace process
         const std::vector<std::string> &arguments,
         bool use_path=true)
     {
-        return sync_execute(context().current_path(cwd).executable(executable).arguments(arguments).use_path(use_path));
+        return sync_execute(
+            context().
+            current_path(cwd).
+            executable(executable).
+            arguments(arguments).
+            use_path(use_path)
+        );
     }
 
     inline int sync_execute(
@@ -30,14 +36,20 @@ namespace bunsan{namespace process
         const std::vector<std::string> &arguments,
         bool use_path=true)
     {
-        return sync_execute(context().current_path(cwd).arguments(arguments).use_path(use_path));
+        return sync_execute(
+            context().
+            current_path(cwd).
+            arguments(arguments).
+            use_path(use_path)
+        );
     }
 
     template <typename ... Args>
     void check_sync_execute(Args &&...args)
     {
-        int exit_status_ = sync_execute(std::forward<Args>(args)...);
+        const int exit_status_ = sync_execute(std::forward<Args>(args)...);
         if (exit_status_)
-            BOOST_THROW_EXCEPTION(bunsan::process::non_zero_exit_status_error(exit_status_));
+            BOOST_THROW_EXCEPTION(
+                bunsan::process::non_zero_exit_status_error(exit_status_));
     }
 }}
