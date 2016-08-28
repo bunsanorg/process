df3b3dc2d5e9c83a3b0ef3ca1bf74cd03d1dc07d
Aleksey Filippov <sarum9in@gmail.com>
2016-08-28 19:55:21 +0400

    Wrapped process::find_executable_in_path() into nested exception block.

--- src/process/unix/path.cpp
+++ src/process/unix/path.cpp
@@ -13,39 +13,48 @@ namespace process {
 
 boost::filesystem::path find_executable_in_path(
     const boost::filesystem::path &executable) {
-  if (executable != executable.filename())
-    BOOST_THROW_EXCEPTION(
-        non_basename_executable_error()
-        << non_basename_executable_error::executable(executable));
+  try {
+    if (executable != executable.filename())
+      BOOST_THROW_EXCEPTION(
+          non_basename_executable_error()
+          << non_basename_executable_error::executable(executable));
 
-  const char *const envpath = ::getenv("PATH");
-  if (!envpath) BOOST_THROW_EXCEPTION(no_environment_path_error());
+    const char *const envpath = ::getenv("PATH");
+    if (!envpath) BOOST_THROW_EXCEPTION(no_environment_path_error());
 
-  /*const*/ std::string path(envpath);
-  if (path.empty()) BOOST_THROW_EXCEPTION(empty_environment_path_error());
+    /*const*/ std::string path(envpath);
+    if (path.empty()) BOOST_THROW_EXCEPTION(empty_environment_path_error());
 
 #if defined(__CYGWIN__)
-  if (!::cygwin_posix_path_list_p(path.c_str()))
-  {
-    const int size = ::cygwin_win32_to_posix_path_list_buf_size(path.c_str());
-    std::vector<char> cygpath(size);
-    ::cygwin_win32_to_posix_path_list(path.c_str(), cygpath.data());
-    path = cygpath.data();
-  }
+    if (!::cygwin_posix_path_list_p(path.c_str())) {
+      const int size = ::cygwin_win32_to_posix_path_list_buf_size(path.c_str());
+      std::vector<char> cygpath(size);
+      ::cygwin_win32_to_posix_path_list(path.c_str(), cygpath.data());
+      path = cygpath.data();
+    }
 #endif
 
-  std::vector<std::string> path_dirs;
-  boost::algorithm::split(path_dirs, path, boost::algorithm::is_any_of(":"),
-                          boost::algorithm::token_compress_on);
+    std::vector<std::string> path_dirs;
+    boost::algorithm::split(path_dirs, path, boost::algorithm::is_any_of(":"),
+                            boost::algorithm::token_compress_on);
 
-  for (const boost::filesystem::path dir : path_dirs) {
-    const boost::filesystem::path abs_path =
-        boost::filesystem::absolute(dir / executable);
-    if (boost::filesystem::exists(abs_path)) return abs_path;
+    for (const boost::filesystem::path dir : path_dirs) {
+      const boost::filesystem::path abs_path =
+          boost::filesystem::absolute(dir / executable);
+      if (boost::filesystem::exists(abs_path)) return abs_path;
+    }
+
+    BOOST_THROW_EXCEPTION(
+        no_executable_in_path_error()
+        << no_executable_in_path_error::executable(executable));
+  } catch (find_executable_in_path_error &) {
+    throw;
+  } catch (std::exception &) {
+    BOOST_THROW_EXCEPTION(
+        find_executable_in_path_error()
+        << find_executable_in_path_error::executable(executable)
+        << enable_nested_current());
   }
-
-  BOOST_THROW_EXCEPTION(no_executable_in_path_error()
-                        << no_executable_in_path_error::executable(executable));
 }
 
 }  // namespace process
--- src/process/win32/path.cpp
+++ src/process/win32/path.cpp
@@ -11,23 +11,33 @@ namespace process {
 
 boost::filesystem::path find_executable_in_path(
     const boost::filesystem::path &executable) {
-  if (executable != executable.filename())
+  try {
+    if (executable != executable.filename())
+      BOOST_THROW_EXCEPTION(
+          non_basename_executable_error()
+          << non_basename_executable_error::executable(executable));
+
+    const boost::filesystem::path exts[] = {"", ".exe", ".com", ".bat"};
+    for (const auto &ext : exts) {
+      wchar_t buf[MAX_PATH];
+      LPWSTR dummy;
+      const DWORD size = ::SearchPathW(nullptr, executable.c_str(), ext.c_str(),
+                                       MAX_PATH, buf, &dummy);
+      BOOST_ASSERT(size < MAX_PATH);
+      if (size > 0) return buf;
+    }
+
     BOOST_THROW_EXCEPTION(
-        non_basename_executable_error()
-        << non_basename_executable_error::executable(executable));
-
-  const boost::filesystem::path exts[] = {"", ".exe", ".com", ".bat"};
-  for (const auto &ext : exts) {
-    wchar_t buf[MAX_PATH];
-    LPWSTR dummy;
-    const DWORD size = ::SearchPathW(nullptr, executable.c_str(), ext.c_str(),
-                                     MAX_PATH, buf, &dummy);
-    BOOST_ASSERT(size < MAX_PATH);
-    if (size > 0) return buf;
+        no_executable_in_path_error()
+        << no_executable_in_path_error::executable(executable));
+  } catch (find_executable_in_path_error &) {
+    throw;
+  } catch (std::exception &) {
+    BOOST_THROW_EXCEPTION(
+        find_executable_in_path_error()
+        << find_executable_in_path_error::executable(executable)
+        << enable_nested_current());
   }
-
-  BOOST_THROW_EXCEPTION(no_executable_in_path_error()
-                        << no_executable_in_path_error::executable(executable));
 }
 
 }  // namespace process
